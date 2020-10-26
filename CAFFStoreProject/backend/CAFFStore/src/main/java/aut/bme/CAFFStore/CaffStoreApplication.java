44913b37abe60db6ea31d69c86f63b535415531d
Kurdi Botond <kurdi.boti@gmail.com>
2020-10-26 17:26:10 +0100

    inital basic spring boot application added

---
+++ CAFFStoreProject/backend/CAFFStore/src/main/java/aut/bme/CAFFStore/CaffStoreApplication.java
@@ -0,0 +1,13 @@
+package aut.bme.CAFFStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaffStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaffStoreApplication.class, args);
+	}
+
+}
---
+++ CAFFStoreProject/backend/CAFFStore/src/test/java/aut/bme/CAFFStore/CaffStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package aut.bme.CAFFStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaffStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
