7bd789d51e3478916b51e230b48246c13caa9ce2
Urbán Norbert <urban.norbert.bme@gmail.com>
2020-10-30 10:48:25 +0100

    add registration inputs to web

--- CAFFStoreProject/frontend/caff-store/src/app/app.module.ts
+++ CAFFStoreProject/frontend/caff-store/src/app/app.module.ts
@@ -3,9 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from "@angular/material/icon";
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RegistrationComponent } from './component/registration/registration.component';
 import { LoginComponent } from './component/login/login.component';
+import { MatPasswordStrengthModule } from '@angular-material-extensions/password-strength';
 
 @NgModule({
   declarations: [
@@ -16,7 +24,15 @@ import { LoginComponent } from './component/login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatPasswordStrengthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- CAFFStoreProject/frontend/caff-store/src/app/component/registration/registration.component.ts
+++ CAFFStoreProject/frontend/caff-store/src/app/component/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ValidationErrors, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +7,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
+  hidePassword = true;
+  hideRepeatPassword = true;
+  showDetails: boolean = true;
+  name: string;
+  email: string;
+  password: string;
+  nameFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(2)
+  ]);
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(8),
+    Validators.maxLength(30)
+  ]);
+  registerForm: FormGroup;
+  emailRegex = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,20}$';
 
-  constructor() { }
+  constructor(
+    private _formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.registerForm = this._formBuilder.group({
+      personName: ['', [
+        Validators.required,
+        Validators.pattern('^[a-zA-ZáéíóöőúűÁÉÍÓÖŐÚŰ .-]*$'),
+        Validators.minLength(2),
+      ]],
+      email: ['', [
+        Validators.required,
+        Validators.pattern(this.emailRegex)
+      ]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onStrengthChanged(strength: number) {
+    console.log('password strength = ', strength);
+  }
+
+  register() {
+    console.log(this.name, this.email);
   }
 
 }
