1129126b3fbb885c274fbe3a10a498d9beb101aa
Kurdi Botond <kurdi.boti@gmail.com>
2020-11-13 21:44:05 +0100

    UserDetailsDTO added and UserDTO simplified

--- CAFFStoreProject/backend/CAFFStore/src/main/java/aut/bme/CAFFStore/data/dto/UserDTO.java
+++ CAFFStoreProject/backend/CAFFStore/src/main/java/aut/bme/CAFFStore/data/dto/UserDTO.java
@@ -1,14 +1,11 @@
 package aut.bme.CAFFStore.data.dto;
 
-import aut.bme.CAFFStore.data.entity.Comment;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-
-import java.util.List;
 
 @EqualsAndHashCode
 @ToString
@@ -22,25 +19,11 @@ public class UserDTO {
     @Setter
     private String username;
 
-    @Getter
-    @Setter
-    private String email;
-
-    @Getter
-    @Setter
-    private List<Comment> comments;
-
-    //TODO: private List<ZipFile> caff;
-
     @JsonCreator
     public UserDTO(@JsonProperty("id") String id,
-                   @JsonProperty("username") String username,
-                   @JsonProperty("email") String email,
-                   @JsonProperty("id") List<Comment> comments) {
+                   @JsonProperty("username") String username) {
 
         this.id = id;
         this.username = username;
-        this.email = email;
-        this.comments = comments;
     }
 }
---
+++ CAFFStoreProject/backend/CAFFStore/src/main/java/aut/bme/CAFFStore/data/dto/UserDetailsDTO.java
@@ -0,0 +1,42 @@
+package aut.bme.CAFFStore.data.dto;
+
+import aut.bme.CAFFStore.data.entity.Comment;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+public class UserDetailsDTO {
+
+    @Getter
+    @Setter
+    private String id;
+
+    @Getter
+    @Setter
+    private String username;
+
+    @Getter
+    @Setter
+    private String email;
+
+    @Getter
+    @Setter
+    private List<Comment> comments;
+
+    //TODO: private List<ZipFile> caff;
+
+    @JsonCreator
+    public UserDetailsDTO(@JsonProperty("id") String id,
+                          @JsonProperty("username") String username,
+                          @JsonProperty("email") String email,
+                          @JsonProperty("id") List<Comment> comments) {
+
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.comments = comments;
+    }
+}
