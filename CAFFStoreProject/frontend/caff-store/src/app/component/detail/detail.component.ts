ff423c60047378f73845b9b469390d1ebc9f93b7
Urbán Norbert <urban.norbert.bme@gmail.com>
2020-11-25 10:29:18 +0100

    add comment section to details page

--- CAFFStoreProject/frontend/caff-store/src/app/component/detail/detail.component.ts
+++ CAFFStoreProject/frontend/caff-store/src/app/component/detail/detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { Caff } from 'src/app/model/caff';
 import { NetworkService } from 'src/app/service/network/network.service';
@@ -10,10 +11,12 @@ import { NetworkService } from 'src/app/service/network/network.service';
 })
 export class DetailComponent implements OnInit {
   currentCaff: Caff;
+  comments: Array<string>;
 
   constructor(
     private _router: ActivatedRoute,
-    private _network: NetworkService
+    private _network: NetworkService,
+    private _sanitization: DomSanitizer
   ) {
   }
 
@@ -22,6 +25,14 @@ export class DetailComponent implements OnInit {
       console.log(params['id']);
       this._network.details(params['id']).then(data => {
         console.log(data);
+        var url = 'data:image/GIF;base64,' + encodeURIComponent(data.gifFile);
+        var image = this._sanitization.bypassSecurityTrustResourceUrl(url);
+        this.currentCaff = new Caff(data.id, data.originalFileName, image);
+        this.comments = new Array();
+        data.comments.forEach(element => {
+          console.log("comment: ", element.comment);
+          this.comments.push(element.comment);
+        });
       }).catch(err => {
         console.log(err);
       });
